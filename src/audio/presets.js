47a024bb92fe0ac82ca35f2d9b6014a1206d4f28
Alexander Wallin <office@alexanderwallin.com>
2017-09-13 19:55:11 +0100

    Makes MILD aid a little gentler

--- src/audio/presets.js
+++ src/audio/presets.js
@@ -9,7 +9,7 @@ export default {
   },
   [SimulatorType.AID]: {
     [HearingLossGrade.NONE]: [0, 0, 0, 0, 0, 0, 0],
-    [HearingLossGrade.MILD]: [7, 12, 15, 22, 25, 25, 25],
+    [HearingLossGrade.MILD]: [4, 8, 10, 15, 18, 18, 18],
     [HearingLossGrade.MODERATE]: [22, 27, 30, 37, 40, 40, 40],
     [HearingLossGrade.SEVERE]: [30, 42, 45, 52, 55, 55, 55],
   },
