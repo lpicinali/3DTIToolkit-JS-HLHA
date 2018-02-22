6e45050c5255f3d89c38db30e7cd63d478901375
Alexander Wallin <office@alexanderwallin.com>
2018-02-22 13:33:28 +0100

    Changes font to (antialiased) PT Sans

--- src/containers/App.js
+++ src/containers/App.js
@@ -16,7 +16,27 @@ import { GutteredElement } from 'src/styles/grid.js'
 import { MAX_WIDTH } from 'src/styles/layout.js'
 
 injectGlobal`
-  @import url('https://fonts.googleapis.com/css?family=Roboto:400,700');
+  @font-face {
+    font-family: 'PT-Sans';
+    src:
+      local('PT Sans'),
+      local('PTSans-Regular'),
+      url('/assets/fonts/PT_Sans-Web-Regular.ttf');
+    font-style: normal;
+    font-weight: normal;
+    text-rendering: optimizeLegibility;
+  }
+
+  @font-face {
+    font-family: 'PT-Sans';
+    src:
+      local('PT Sans Bold'),
+      local('PTSans-Bold'),
+      url('/assets/fonts/PT_Sans-Web-Bold.ttf');
+    font-style: normal;
+    font-weight: bold;
+    text-rendering: optimizeLegibility;
+  }
 
   * {
     box-sizing: border-box;
@@ -29,7 +49,8 @@ injectGlobal`
 
   body {
     background-color: #f7f7f7;
-    font-family: 'Roboto', sans-serif;
+    font-family: 'PT-Sans', sans-serif;
+    -webkit-font-smoothing: antialiased;
   }
 `
 
