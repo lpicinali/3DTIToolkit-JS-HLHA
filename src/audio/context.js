3c97dcb5a08a59e3a4bfd5cfd53386b14d9bdccd
Alexander Wallin <office@alexanderwallin.com>
2018-06-19 16:59:26 +0200

    Fixes audio context resume bug

--- src/audio/context.js
+++ src/audio/context.js
@@ -1,4 +1,8 @@
+import StartAudioContext from 'startaudiocontext'
+
 const AudioContext = window.AudioContext || window.webkitAudioContext
 const context = new AudioContext()
+
+StartAudioContext(context)
 
 export default context
