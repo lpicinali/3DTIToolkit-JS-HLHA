fc7c3090ce94057f952793102a598b009ff110b5
Alexander Wallin <office@alexanderwallin.com>
2019-01-21 16:37:36 +0100

    Bumps persisted state version to 3

--- src/store/createPersistorEnhancer.js
+++ src/store/createPersistorEnhancer.js
@@ -1,6 +1,6 @@
 import persistState from 'redux-localstorage'
 
-const STORAGE_KEY = '3dti_online_toolkit_2'
+const STORAGE_KEY = '3dti_online_toolkit_3'
 
 export default function createPersistorEnhancer() {
   return persistState(['alerts'], { key: STORAGE_KEY })
